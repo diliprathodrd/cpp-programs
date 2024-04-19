fe6018209f7edecce3071c11cad4dda7e5cf97bc
diliprathodrd <diliprathodofficial@gmail.com>
2024-04-20 00:38:01 +0530

    chapter 7 - operators

---
+++ Chapter 7 - Operators/ternaryOperator.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5, b = 3, max;
+    
+    max = (a > b) ? a : b;
+    
+    cout << "Maximum value: " << max << endl;
+
+    return 0;
+}
